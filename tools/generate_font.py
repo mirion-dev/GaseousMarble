008a2ce2a7f1f11db3c6b803f048c0375c62baa0
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-01-14 12:09:49 +0800

    fix inverted logic of dense

--- tools/generate_font.py
+++ tools/generate_font.py
@@ -65,39 +65,42 @@ def generate_font(
     if not smoothing:
         draw0.fontmode = '1'
 
-    line_length = 0
+    glyph_spacing = 0 if dense else 1
+    line_width = 0
     min_top = 0
     max_bottom = 0
-    max_width = 0
+    max_glyph_width = 0
     for (font, chars) in zip(fonts, chars_map.values()):
         draw0.font = font
         for ch in chars:
             (l, t, r, b) = map(round, draw0.textbbox((0, 0), ch, stroke_width=stroke_width))
             w = r - l + shadow_offset
-            line_length += w + dense
+            line_width += w + glyph_spacing
             min_top = min(min_top, t)
             max_bottom = max(max_bottom, b + shadow_offset)
-            max_width = max(max_width, w)
+            max_glyph_width = max(max_glyph_width, w)
 
-    line_length = line_length - dense
-    line_height = max_bottom - min_top + dense
+    line_width -= glyph_spacing
+    line_height = max_bottom - min_top + glyph_spacing
 
     # calculate the sprite size to arrange glyphs into a roughly square
-    max_line_length = max(math.ceil((line_height + math.sqrt(line_height * (line_height + 4 * line_length))) / 2), max_width)  # x == h * (l / x + 1)
-    line_length = 0
+    sprite_width = max(math.ceil((line_height + math.sqrt(line_height * (line_height + 4 * line_width))) / 2), max_glyph_width)  # x == h * (l / x + 1)
+    line_width = 0
     line_count = 1
     for (font, chars) in zip(fonts, chars_map.values()):
         draw0.font = font
         for ch in chars:
             (l, t, r, b) = map(round, draw0.textbbox((0, 0), ch, stroke_width=stroke_width))
             w = r - l + shadow_offset
-            if line_length + w > max_line_length:
-                line_length = 0
+            if line_width + w > sprite_width:
+                line_width = 0
                 line_count += 1
-            line_length += w + dense
+            line_width += w + glyph_spacing
+
+    sprite_height = line_height * line_count - glyph_spacing
 
     # generates the font sprite and glyph data
-    image = Image.new('RGBA', (max_line_length, line_height * line_count - dense))
+    image = Image.new('RGBA', (sprite_width, sprite_height))
     draw = ImageDraw.Draw(image)
     if not smoothing:
         draw.fontmode = '1'
@@ -105,7 +108,7 @@ def generate_font(
     data_path = os.path.splitext(sprite_path)[0] + '.gly'
     os.makedirs(os.path.dirname(data_path), exist_ok=True)
     with open(data_path, 'wb+') as file:
-        file.write(b'GLY\x01\x00\x00' + struct.pack('Hh', line_height - dense, min_top))
+        file.write(b'GLY\x01\x00\x00' + struct.pack('Hh', line_height - glyph_spacing, min_top))
 
         x = 0
         y = 0
@@ -116,7 +119,7 @@ def generate_font(
                 (l, t, r, b) = map(round, draw.textbbox((0, 0), ch, stroke_width=stroke_width))
                 w = r - l + shadow_offset
                 a = w + round(draw.textlength(ch) - (raw_r - raw_l))
-                if x + w > max_line_length:
+                if x + w > sprite_width:
                     x = 0
                     y += line_height
                 file.write(struct.pack('IHHHhh', ord(ch), x, y, w, a, raw_l))
@@ -129,6 +132,6 @@ def generate_font(
                 draw.text((draw_x, draw_y), ch, fill, stroke_width=stroke_width, stroke_fill=stroke_fill)
                 draw.text((draw_x, draw_y), ch, fill)  # make glyphs more clear
 
-                x += w + dense
+                x += w + glyph_spacing
 
     image.save(sprite_path)
