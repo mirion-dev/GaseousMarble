dd513bcc98b8bf407d78e94b03c79805bff3a891
Mirion <51433180+mirion-dev@users.noreply.github.com>
2026-06-19 14:22:09 +0800

    Fix inconsistent glyph metrics

--- tools/generate_font/generate_font.py
+++ tools/generate_font/generate_font.py
@@ -136,11 +136,10 @@ def generate_font(
                 raw_a = round(draw.textlength(ch))
                 (l, t, r, b) = bbox(draw, ch)
                 w = r - l
-                a = w + raw_a - raw_w
                 if x + w > sprite_width:
                     x = 0
                     y += line_height + glyph_spacing
-                data.write(struct.pack('IHHHhh', ord(ch), x, y, w, a, raw_l))
+                data.write(struct.pack('IHHHhh', ord(ch), x, y, raw_w, raw_a, raw_l))
 
                 draw_x = x - l
                 draw_y = y - min_glyph_top
